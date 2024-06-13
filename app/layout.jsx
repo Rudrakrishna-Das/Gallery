2d6197e8b0d6a4a345d5c3d4d8aecaf7826fc2e2
Rudrakrishna-Das <rudradas16996@gmail.com>
2024-06-13 15:21:00 +0530

    bug fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,9 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <UserProvider>{children}</UserProvider>
+        <Footer />
       </body>
-
-      <Footer />
     </html>
   );
 }
